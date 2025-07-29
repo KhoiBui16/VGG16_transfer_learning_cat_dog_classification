de5896ccfbe65ed3048f7edd87c019947104feb3
KhoiBui16 <khoib1601@gmail.com>
2025-07-30 00:48:59 +0700

    upload data and sources

---
+++ test.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
